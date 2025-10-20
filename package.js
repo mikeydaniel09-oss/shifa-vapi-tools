c41bc7cbdcc532b6bca1a348aea0a575ecf73012
mikeydaniel09-oss <mikeydaniel09@gmail.com>
2025-10-20 13:08:06 -0500

    Add files via upload

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "timenow-service",
+  "version": "1.0.0",
+  "description": "Simple Express server for returning current date/time in JSON for Vapi.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
